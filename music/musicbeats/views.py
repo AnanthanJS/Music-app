955840669fb186e42e042dd11c59f0b394e105f4
AnanthanJS <ananthakrishnanjs816@gmail.com>
2024-04-24 16:58:44 +0530

    remaining code 1

--- music/musicbeats/views.py
+++ music/musicbeats/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Song
 
 # Create your views here.
 def home(request):
-    return render(request, 'musicbeats/index.html')
+    song = Song.objects.all()
+    return render(request, 'musicbeats/index.html', {'song': song})
